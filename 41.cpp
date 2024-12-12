9ea48b829219dea2305f7b2b57336880aae9a3ea
Ralliyah889 <ralliyahrushdah@gmail.com>
2024-12-12 10:15:12 +0530

    Add files via upload

---
+++ 41.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s1="anushree";
+    string r=s1.substr(3,2);
+    cout<<"string is:"<<r;
+    return 0;
+    
+}
